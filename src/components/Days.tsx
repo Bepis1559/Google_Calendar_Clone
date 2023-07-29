c0e4bee2ba314f4e9e10ed34dfaa2334bc4b2ec7
Bepis1559 <axl2001@abv.bg>
2023-07-29 12:10:23 +0300

    added basic event modal

--- src/components/Days.tsx
+++ src/components/Days.tsx
@@ -1,5 +1,5 @@
 import { useAtom } from "jotai";
-import { type ReactElement, useId, useRef } from "react";
+import { type ReactElement, useId, useRef, Fragment, useState } from "react";
 import { todaysAtom } from "../contexts/calendar";
 import { format } from "date-fns";
 import { handleVisibleDates } from "../helpers/handleVisibleDates";
@@ -7,37 +7,49 @@ import {
   handleDayClasses,
   handleNumberInDayClasses,
 } from "../helpers/handleClasses";
+import { AddEventModal } from "./AddEventModal";
 
 export function Days(): ReactElement {
-  const unMutableToday = useRef(new Date());
   const [today] = useAtom(todaysAtom);
+  const [isEventModalOpened, setIsEventModalOpened] = useState(false);
+  const unMutableToday = useRef(new Date());
   const visibleDates = handleVisibleDates(today);
   const id = useId();
+
+  const handleOpenEventModal = () => setIsEventModalOpened(true);
+  const handleCloseEventModal = () => setIsEventModalOpened(false);
 
   return (
     <div className="days">
       {visibleDates.map((visibleDate) => {
         return (
           // the whole day card
-          <div
-            key={`${id}--${format(visibleDate, "yyyy-MM-dd")}`}
-            className={handleDayClasses(
-              today,
-              visibleDate,
-              unMutableToday.current,
-            )}>
-            <div className="day-header">
-              {/* day name as a day of the week  */}
-              <div className="week-name">{format(visibleDate, "EEE")}</div>
-              {/* the current day as a number */}
-              <div className={handleNumberInDayClasses(visibleDate)}>
-                {visibleDate.getDate()}
+          <Fragment key={`${id}--${format(visibleDate, "yyyy-MM-dd")}`}>
+            {isEventModalOpened ? (
+              <AddEventModal handleCloseEventModal={handleCloseEventModal} />
+            ) : null}
+            <div
+              className={handleDayClasses(
+                today,
+                visibleDate,
+                unMutableToday.current,
+              )}>
+              <div className="day-header">
+                {/* day name as a day of the week  */}
+                <div className="week-name">{format(visibleDate, "EEE")}</div>
+                {/* the current day as a number */}
+                <div className={handleNumberInDayClasses(visibleDate)}>
+                  {visibleDate.getDate()}
+                </div>
+                <button
+                  onClick={handleOpenEventModal}
+                  type="button"
+                  className="add-event-btn">
+                  +
+                </button>
               </div>
-              <button type="button" className="add-event-btn">
-                +
-              </button>
             </div>
-          </div>
+          </Fragment>
         );
       })}
     </div>
