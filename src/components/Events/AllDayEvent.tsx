4181c408248c41aa9a371406edc156c110ec3367
Bepis1559 <axl2001@abv.bg>
2023-08-03 17:41:32 +0300

    .

--- src/components/Events/AllDayEvent.tsx
+++ src/components/Events/AllDayEvent.tsx
@@ -1,10 +1,15 @@
 import { type ReactElement } from "react";
+// import { handleEventModal } from "../../helpers/handleEventModal";
+// import { AddEventModal } from "../Modal/AddEventModal";
 
 export function AllDayEvent(props: allDayEvent): ReactElement {
   const { eventColor, eventName } = props;
 
   return (
-    <button type="button" className={`all-day-event ${eventColor} event`}>
+    <button
+      // onClick={() => <AddEventModal date={""} handleCloseEventModal={handleEventModal()}/>}
+      type="button"
+      className={`all-day-event ${eventColor} event`}>
       <div className="event-name">{eventName}</div>
     </button>
   );
