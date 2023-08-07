716886f5480e2a22cb3e15d5dc2f48c6c72e686d
Bepis1559 <axl2001@abv.bg>
2023-08-07 10:59:32 +0300

    fixed buttons

--- src/components/Modal/Form/AddButton.tsx
+++ src/components/Modal/Form/AddButton.tsx
@@ -3,7 +3,7 @@ import { type ReactElement } from "react";
 export function AddButton(): ReactElement {
   return (
     <>
-      <button className={`btn btn-success`} type="submit">
+      <button className="btn btn-success" type="submit">
         Add
       </button>
     </>
--- src/components/Modal/Form/SaveButton.tsx
+++ src/components/Modal/Form/SaveButton.tsx
@@ -3,7 +3,7 @@ import { type ReactElement } from "react";
 export function SaveButton(): ReactElement {
   return (
     <>
-      <button className={`btn btn-success`} type="submit">
+      <button className="btn btn-success" type="submit">
         Save
       </button>
     </>
