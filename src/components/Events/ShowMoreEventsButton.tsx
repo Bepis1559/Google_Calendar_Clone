43678a5baf95af6b94cb740d1381e4c6e1f24af7
Bepis1559 <axl2001@abv.bg>
2023-08-12 15:53:34 +0300

    added ShowMoreEventsButton.tsx component

---
+++ src/components/Events/ShowMoreEventsButton.tsx
@@ -0,0 +1,11 @@
+import { type ReactElement } from "react";
+
+export function ShowMoreEventsButton(): ReactElement {
+  return (
+    <>
+      <button type="button" className="events-view-more-btn">
+        +2 More
+      </button>
+    </>
+  );
+}
